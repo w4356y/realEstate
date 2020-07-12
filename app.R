71fc220fd6bfb6c53f32f6c0907c70a016c72b32
w4355y <w4356y@163.com>
2020-07-12 10:33:00 +0800

    Initial commit

---
+++ app.R
@@ -0,0 +1,8 @@
+# Launch the ShinyApp (Do not remove this comment)
+# To deploy, run: rsconnect::deployApp()
+# Or use the blue button on top of this file
+pkgload::load_all()
+Sys.setlocale(category = "LC_ALL", locale = "chs")
+options( "golem.app.prod" = TRUE)
+#source('R/run_app.R')
+run_app()
